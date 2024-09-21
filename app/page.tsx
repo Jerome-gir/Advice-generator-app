56422b36cc116b0d9b13d6c8cd1ae148ec894763
Jerome <jerome.gir@gmail.com>
2024-09-21 19:37:34 +0200

    desktop version

--- app/page.tsx
+++ app/page.tsx
@@ -19,23 +19,34 @@ export default function Home() {
   return (
     <>
       <div className="bg-darkBlue h-screen flex justify-center items-center">
-        <div className="size-96 bg-darkGrayishBlue rounded-xl relative">
+        <div className="size-96 sm:w-[600px] bg-darkGrayishBlue rounded-xl relative">
           <div className="flex flex-col items-center justify-between gap-10 p-8">
-            <h1 className="text-neonGreen text-sm tracking-widest uppercase">
+            <h1 className="text-neonGreen text-sm sm:text-base tracking-widest uppercase">
               Advice # {advice.id}
             </h1>
-            <p className="text-xl font-extrabold text-ligthCyan text-center">
+            <p className="text-xl sm:text-2xl font-extrabold text-ligthCyan text-center">
+              <span className="text-3xl">“</span>
               {advice.advice}
+              <span className="text-3xl">”</span>
             </p>
             <Image
+              className="block sm:hidden"
               src="/pattern-divider-mobile.svg"
-              alt="image de séparation"
+              alt="image de séparation mobile"
               width={295}
+              height={16}
+            />
+            <Image
+              className="hidden sm:block"
+              src="/pattern-divider-desktop.svg"
+              alt="image de séparation desktop"
+              width={490}
               height={16}
             />
             <button
               onClick={fetchAdvice}
-              className="rounded-full size-20 bg-neonGreen flex items-center justify-center absolute -bottom-10"
+              className="rounded-full size-20 bg-neonGreen flex items-center justify-center absolute -bottom-10
+               sm:transition-all sm:duration-500 sm:hover:shadow-[0_0_25px_5px] sm:hover:shadow-neonGreen"
             >
               <Image
                 src="/icon-dice.svg"
